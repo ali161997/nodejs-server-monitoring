5b6bc833d40b8b11d2f87a103e451446b243017f
ali-hashem-hamed <ali.hashem@advintic.com>
2022-02-13 18:21:10 +0200

    second commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
+/*jshint esversion: 6 */
 import express from 'express';
 const app = express();
-import http from 'http';
+import http, { STATUS_CODES } from 'http';
 import eventLoopStats from 'event-loop-stats';
 import { resourceUsage, cpuUsage, memoryUsage } from 'process';
 import json from 'body-parser';
@@ -11,29 +12,51 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const usersRoom = 12345;
+const adminRoom = 67891;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get('/user', (req, res) => {
+  res.sendFile(__dirname + '/user.html');
+});
+app.get('/admin', (req, res) => {
+  res.sendFile(__dirname + '/admin.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on('disconnect', (reason) => {
-    console.log('user disconnected');
-    console.log(reason);
+  socket.on('joinAdmin', ({ name }, callback) => {
+    socket.join(adminRoom);
+    console.log('admin joined', name);
+    socket.on('disconnect', (reason) => {
+      console.log(`${name} disconnected from admin =>${reason}`);
+    });
+    setInterval(() => {
+      // **from library processs
+      socket.to(adminRoom).emit('data', {
+        resources: resourceUsage(),
+        cpu: cpuUsage(),
+        memory: memoryUsage(),
+        eventloop: eventLoopStats.sense(),
+      });
+    }, 5000);
   });
-  // ** send current memory and cpu usage usage for every 500 millisconds
-  setInterval(() => {
-    // **from library processs
-    io.emit('resource', resourceUsage());
-    io.emit('cpu', cpuUsage());
-    io.emit('memory', memoryUsage());
-    // ** from event-loop-stats package
-    io.emit('eventloop', eventLoopStats.sense());
-  }, 5000);
+  socket.on('joinUser', ({ name }, callback) => {
+    console.log('joined', name);
+    socket.join(usersRoom);
+    io.to(adminRoom).emit('join', {
+      name,
+    });
+    socket.on('disconnect', (reason) => {
+      console.log('disconnection', reason, name);
+      console.log(`${name} disconnected from users =>${reason}`);
+      io.to(adminRoom).emit('leave', {
+        name: name,
+        reason: reason,
+      });
+    });
+  });
+  // ** send current memory and cpu usage usage for every 5000 millisconds
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(8000, () => {
+  console.log('listening on *:8000');
 });
